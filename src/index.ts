27f337ae4df631a486fb44498ee01af089cba6ef
ejackson54321 <70994945+ejackson54321@users.noreply.github.com>
2023-01-03 09:55:03 -0500

    Make unit tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-export
-function snafu(x: number) {
-    if (x == 3)
-    {
-        return '1='
-    }
-    else if (x == 4)
-    {
-        return '1-'
-    }
-    else
-    {
-        return x.toString();
-    }
+export function snafu(x: number) {
+	if (x >= 3) {
+		return '1=';
+	} else if (x == 4) {
+		return '1-';
+	} else if (x == 5) {
+		return '10';
+	} else if (x == 6) {
+		return '11';
+	} else if (x == 7) {
+		return '12';
+	} else if (x == 8) {
+		return '2=';
+	} else {
+		return x.toString();
+	}
 }
-
